f577f4fce023ca87fe6bf34489617db78ee59be2
Anders Oscarsen <andersoscarsen@gmail.com>
2011-06-06 21:13:31 +0200

    further separation of vim and py code

--- src/slem.py
+++ src/slem.py
@@ -1,4 +1,6 @@
 import vim
+import os
+import pipes
 
 def move_back(coords):
     '''Returns a position one step backwards in the buffer. should the current
@@ -200,3 +202,21 @@ def ask_vars(screen=False, window=False):
         vim.command('let __slem_wd = input("window number: ", "' +
             SLEM_VARS['window'] + '")')
         SLEM_VARS['window'] = vim.eval('__slem_wd')
+
+def vim_slem(to_line):
+    if len(SLEM_VARS['screen']) < 1:
+        ask_vars(screen=True, window=True)
+    file_extension = vim.current.buffer.name.rsplit('.',1)[1]
+    block = get_blocking_fn({
+        'filetype':file_extension,
+        'to_line':vim.eval(to_line)
+        })(vim.current.window.cursor)
+    lines = extract(block[0], block[1])
+    text = '\n'.join(lines) + '\n'
+    if file_extension == 'py' and lines[-1][0].isspace():
+        text += '\n'
+    text = pipes.quote(text)
+    message = 'screen -S ' + SLEM_VARS['screen']
+    message += ' -p ' + SLEM_VARS['window']
+    message += ' -X stuff ' + text 
+    os.system(message)
